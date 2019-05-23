093fbed9e1baff5c9ac92a366be313e68aabb6b7
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-23 12:33:07 +0200

    a bit better

--- tests/testthat/test-cran-package-list.R
+++ tests/testthat/test-cran-package-list.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("cran_package_list works", {
+  expect_is(cran_package_list(), "tbl_df")
+})
+
+test_that("cran_active_packages works", {
+  expect_is(cran_active_packages(), "tbl_df")
+})
+
+
+test_that("cran_package_histories works", {
+  expect_is(cran_package_histories(), "tbl_df")
 })
